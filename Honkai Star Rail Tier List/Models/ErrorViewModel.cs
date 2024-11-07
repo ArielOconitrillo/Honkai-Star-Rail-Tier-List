b008311886e82a1a05d1a17ee5aa3acc2e3fc3d0
Ariel <Ariel@DESKTOP-53O9VJ7>
2024-11-07 15:31:56 -0500

    Add project files.

---
+++ Honkai Star Rail Tier List/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Honkai_Star_Rail_Tier_List.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
