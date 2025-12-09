3bb1b73f95aac05f5195bfd8c650252927cb5442
Ariel <Arieloconitrillo@gmail.com>
2025-12-09 17:52:59 -0500

    Created an Accordian for information regarding the tier list.

--- Honkai Star Rail Tier List/wwwroot/js/site.js
+++ Honkai Star Rail Tier List/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/*
+* Code taken for W3Schools: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion_symbol
+*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
