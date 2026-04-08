74dc2dda6279806d9ba104f1f44a03c5b823ed20
Ariel <Arieloconitrillo@gmail.com>
2026-04-08 18:20:15 -0400

    Updated how skills are displayed.

--- Honkai Star Rail Tier List/Data/DbInitializer.cs
+++ Honkai Star Rail Tier List/Data/DbInitializer.cs
@@ -154,7 +154,7 @@ namespace Honkai_Star_Rail_Tier_List.Data
                 CharacterId = firefly.Id,
                 SkillType = "Basic ATK",
                 Name = "Order: Flare Propulsion",
-                Description = "Deals Fire DMG equal to <span data-stat=\"Damage\"></span>% of SAM's ATK to a single target enemy.",
+                Description = "Deals Fire DMG equal to <span class=\"skill-value\" data-stat=\"Damage\"></span>% of SAM's ATK to a single target enemy.",
                 MaxLevel = 7
             },
 
@@ -162,7 +162,7 @@ namespace Honkai_Star_Rail_Tier_List.Data
                 CharacterId = firefly.Id,
                 SkillType = "Basic ATK (Enhanced)",
                 Name = "Fyrefly Type-IV: Pyrogenic Decimation",
-                Description = "Restores HP by an amount equal to 20% of this unit's Max HP. Deals Fire DMG equal to <span data-stat=\"Damage\"></span>% of SAM's ATK to a single target enemy.",
+                Description = "Restores HP by an amount equal to 20% of this unit's Max HP. Deals Fire DMG equal to <span class=\"skill-value\" data-stat=\"Damage\"></span>% of SAM's ATK to a single target enemy.",
                 MaxLevel = 7
             },
 
@@ -171,7 +171,7 @@ namespace Honkai_Star_Rail_Tier_List.Data
                 CharacterId = firefly.Id,
                 SkillType = "Skill",
                 Name = "Order: Aerial Bombardment",
-                Description = "Consumes SAM's HP equal to 40% of SAM's Max HP and regenerates a fixed amount of Energy equal to <span data-stat=\"Energy\"></span>% of SAM's Max Energy. Deals Fire DMG equals to <span data-stat=\"Damage\"></span>% of SAM's ATK to a single target enemy. If the current HP is not sufficient, then SAM's HP is reduced to 1 when using this skill. Enables this unit's next Action to be Advanced by 25%.",
+                Description = "Consumes SAM's HP equal to 40% of SAM's Max HP and regenerates a fixed amount of Energy equal to <span class=\"skill-value\" data-stat=\"Energy\"></span>% of SAM's Max Energy. Deals Fire DMG equals to <span class=\"skill-value\" data-stat=\"Damage\"></span>% of SAM's ATK to a single target enemy. If the current HP is not sufficient, then SAM's HP is reduced to 1 when using this skill. Enables this unit's next Action to be Advanced by 25%.",
                 MaxLevel = 12
             },
 
@@ -179,7 +179,7 @@ namespace Honkai_Star_Rail_Tier_List.Data
                 CharacterId= firefly.Id,
                 SkillType = "Skill (Enhanced)",
                 Name = "Fyrefly Type-IV: Deathstar Overload",
-                Description = "Restores HP by an amount equal to 25% of this unit's Max HP. Applies Fire Weakness to a single target enemy, lasting for 2 turn(s). Deals Fire DMG equal to (0.2 x Break Effect + <span data-stat=\"MainDamage\"></span>%) of SAM's ATK to this target. At the same time, deal Fire DMG equal to (0.10 x Break Effect + <span data-stat=\"AdjacentDamage\"></span>%) of SAM's ATK to adjacent targets. The Break Effect taken into the calculation is capped at 360%.",
+                Description = "Restores HP by an amount equal to 25% of this unit's Max HP. Applies Fire Weakness to a single target enemy, lasting for 2 turn(s). Deals Fire DMG equal to (0.2 x Break Effect + <span class=\"skill-value\" data-stat=\"MainDamage\"></span>%) of SAM's ATK to this target. At the same time, deal Fire DMG equal to (0.10 x Break Effect + <span class=\"skill-value\" data-stat=\"AdjacentDamage\"></span>%) of SAM's ATK to adjacent targets. The Break Effect taken into the calculation is capped at 360%.",
                 MaxLevel= 12
             },
 
@@ -188,7 +188,7 @@ namespace Honkai_Star_Rail_Tier_List.Data
                 CharacterId = firefly.Id,
                 SkillType = "Ultimate",
                 Name = "Fyrefly Type-IV: Complete Combustion",
-                Description = "Upon entering the Complete Combustion state, Advances SAM's Action by 100% and gains Enhanced Basic ATK and Enhanced Skill. While in Complete Combustion, increases SPD by <span data-stat=\"Speed\"></span>, and when using the Enhance Basic ATK or Enhance Skill, increases this unit's Weakness Break efficiency by 50% and Break DMG received by enemy targets by <span data-stat=\"Damage\"></span>%, lasting until the current attack ends.\r\n\r\nA countdown timer for the Complete Combustion state appears on the Action Order. When the countdown turn starts, SAM exits Complete Combustion state. The countdown has a fixed SPD of 70.\r\n\r\nSAM cannot use Ultimate while in Complete Combustion.",
+                Description = "Upon entering the Complete Combustion state, Advances SAM's Action by 100% and gains Enhanced Basic ATK and Enhanced Skill. While in Complete Combustion, increases SPD by <span class=\"skill-value\" data-stat=\"Speed\"></span>, and when using the Enhance Basic ATK or Enhance Skill, increases this unit's Weakness Break efficiency by 50% and Break DMG received by enemy targets by <span class=\"skill-value\" data-stat=\"Damage\"></span>%, lasting until the current attack ends.\r\n\r\nA countdown timer for the Complete Combustion state appears on the Action Order. When the countdown turn starts, SAM exits Complete Combustion state. The countdown has a fixed SPD of 70.\r\n\r\nSAM cannot use Ultimate while in Complete Combustion.",
                 MaxLevel = 12
             },
 
@@ -197,7 +197,7 @@ namespace Honkai_Star_Rail_Tier_List.Data
                 CharacterId = firefly.Id,
                 SkillType = "Talent",
                 Name = "Chrysalid Pyronexus",
-                Description = "The lower the HP, the less DMG received. When HP is 20% or lower, the DMG Reduction reaches its maximum effect, reducing up to <span data-stat=\"DamageReduction\"></span>%. During the Complete Combustion, the DMG Reduction remains at its maximum effect, and the Effect RES increases by <span data-stat=\"EffectRES\"></span>%.\r\nIf Energy is lower than 50% when the battle starts, regenerates Energy to 50%. Once Energy is regenerated to its maximum, dispels all debuffs on this unit.",
+                Description = "The lower the HP, the less DMG received. When HP is 20% or lower, the DMG Reduction reaches its maximum effect, reducing up to <span class=\"skill-value\" data-stat=\"DamageReduction\"></span>%. During the Complete Combustion, the DMG Reduction remains at its maximum effect, and the Effect RES increases by <span data-stat=\"EffectRES\"></span>%.\r\nIf Energy is lower than 50% when the battle starts, regenerates Energy to 50%. Once Energy is regenerated to its maximum, dispels all debuffs on this unit.",
                 MaxLevel = 12
             },
 
@@ -244,7 +244,7 @@ namespace Honkai_Star_Rail_Tier_List.Data
 
             //Basic Atk (Enhanced)
             damageScaling = new double[] {
-                100, 120, 140, 160, 180, 200, 220
+                100, 110, 120, 130, 140, 150, 162.5
             };
 
             skill = context.Skills.First(s => s.Name == "Fyrefly Type-IV: Pyrogenic Decimation");
@@ -270,8 +270,8 @@ namespace Honkai_Star_Rail_Tier_List.Data
             addSkillValues(skillLevels, adjacentDamageScaling, skill.Id, SkillStatType.AdjacentDamage);
 
             //Ultimate
-            double[] speedScaling = new double[] { 10, 11, 12, 13, 14, 15, 16.25, 17.5, 18.75, 20, 21, 22 };
-            damageScaling = new double[] { 30, 33, 36, 39, 42, 45, 48.75, 52.5, 56.25, 60, 63, 66 };
+            double[] speedScaling = new double[] { 30, 33, 36, 39, 42, 45, 48.75, 52.5, 56.25, 60, 63, 66 };
+            damageScaling = new double[] { 10, 11, 12, 13, 14, 15, 16.25, 17.5, 18.75, 20, 21, 22 };
 
             skill = context.Skills.First(s => s.Name == "Fyrefly Type-IV: Complete Combustion");
 
@@ -307,7 +307,7 @@ namespace Honkai_Star_Rail_Tier_List.Data
             {
                 Character = firefly,
                 Strengths = @"Can help keep herself alive with self healing
-                              Has plenty of viable team members, including some easily accesable ones</li>
+                              Has plenty of viable team members, including some easily accesable ones
                               Her E1 helps a lot with skill point management, as her enhanced skill no longer consumes them
                               Best girl",
                 Weaknesses = @"Requires a lot of speed to build properly
